2b10a3971e8acc5696676359224946bcd1a31aa8
Lukas Buncak <519181@student.fontys.nl>
2022-12-25 01:05:50 +0100

    cors policy

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var quotesSofi = require('./quotesSofi');
 const app = require('express')();
+const cors = require("cors");
 const PORT = process.env.PORT || 8080;
+app.use(
+    cors({
+        origin: "http://127.0.0.1:5500",
+    })
+)
 
 app.listen(PORT, () => console.log(`data are succesfuly running`));
 
