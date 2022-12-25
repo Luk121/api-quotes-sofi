19be74d50132328b9c02b2ea9fddf55d272f59ae
Lukas Buncak <519181@student.fontys.nl>
2022-12-25 01:32:30 +0100

    mockup quotes

--- quotesSofi.js
+++ quotesSofi.js
@@ -1,7 +1,7 @@
 quotes = [
-    'heey1',
-    'heey2',
-    'heey3'
+    'Be yourself; everyone else is already taken.',
+    'A room without books is like a body without a soul.',
+    'Two things are infinite: the universe and human stupidity; and I am not sure about the universe.'
 ]
 
 exports.quotes = quotes;
